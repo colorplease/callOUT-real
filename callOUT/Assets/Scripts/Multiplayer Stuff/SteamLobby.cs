6af680878ce4494a2586c31d0c622a5e04625b4b
colorplease <74271202+colorplease@users.noreply.github.com>
2022-03-04 16:10:25 -0800

    TMP Import lmao

---
+++ callOUT/Assets/Scripts/Multiplayer Stuff/SteamLobby.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Steamworks;
+
+public class SteamLobby : MonoBehaviour
+{
+    //Callbacks
+    protected Callback<LobbyCreated_t> LobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> JoinRequest;
+    protected Callback<LobbyEnter_t> LobbyEntered;
+
+    //Variables
+    public ulong CurrentLobbyID;
+    private const string HostAddressKey = "HostAddress";
+    private CustomNetworkManager manager;
+
+    //Gameobjct
+    public GameObject HostButton;
+}
