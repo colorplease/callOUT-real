35c044f76128a70bd1292050724fcd64574a957a
colorplease <74271202+colorplease@users.noreply.github.com>
2022-03-11 17:56:01 -0800

    question mark 2.0

--- callOUT/Assets/Scripts/Player Stuff/PlayerControllerRedux.cs
+++ callOUT/Assets/Scripts/Player Stuff/PlayerControllerRedux.cs
@@ -6,45 +6,205 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControllerRedux : NetworkBehaviour
 {
+    float playerHeight = 2f;
+
+    [SerializeField] Transform orientation;
+
     [Header("Movement")]
-    public float speed = 0.1f;
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float airMultiplier = 0.4f;
+    [SerializeField] float jumpForce = 5f;
+    float movementMultiplier = 10f;
+    [SerializeField]float gravity;
+    [SerializeField] bool isSprinting;
+    [SerializeField] bool isCrouching;
+    
+
+
+    [Header("Sprinting")]
+    [SerializeField] float walkSpeed = 4f;
+    [SerializeField] float sprintSpeed = 6f;
+    [SerializeField] float acceleration = 10f;
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] KeyCode crouchKey = KeyCode.C;
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Drag")]
+    [SerializeField] float groundDrag = 6f;
+    [SerializeField] float airDrag = 2f;
+
+    float horizontalMovement;
+    float verticalMovement;
+
+    [Header("Ground Detection")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float groundDistance = 0.2f;
+    public bool isGrounded { get; private set; }
+
+    [Header("Crouching")]
+    [SerializeField]Vector3 startingScale;
+    [SerializeField]Vector3 crouchScale;
+    [SerializeField]float crouchSpeed;
+
+    Vector3 moveDirection;
+    Vector3 slopeMoveDirection;
+
+    Rigidbody rb;
+
+    RaycastHit slopeHit;
     [Header("Network")]
     public GameObject PlayerModel;
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayerModel.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(SceneManager.GetActiveScene().name == "TESTING")
-        {
-            if (PlayerModel.activeSelf == false)
-            {
-                PlayerModel.SetActive(true);
-            }
-        }
-        if (hasAuthority)
-        {
-            Movement();
-        }
-    }
-
+    
     public void SetPosition()
     {
         Transform spawnPoint = GameObject.Find("Player 1 Spawn").transform;
         transform.position = spawnPoint.position;
     }
 
-    void Movement()
+    private bool OnSlope()
     {
-       float xDir = Input.GetAxis("Horizontal");
-       float zDIR = Input.GetAxis("Vertical");
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
 
-       Vector3 moveDirection = new Vector3(xDir, 0.0f, zDIR);
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        PlayerModel.SetActive(false);
+    }
 
-       transform.position += moveDirection * speed; 
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "TESTING")
+        {
+            if (PlayerModel.activeSelf == false)
+            {
+                SetPosition();
+                PlayerModel.SetActive(true);
+            }
+        }
+        if (hasAuthority)
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            MyInput();
+            ControlDrag();
+            ControlSpeed();
+            Crouch();
+            Jump();
+            slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
+            
+        }
+    }
+
+    void MyInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(sprintKey) && !isCrouching)
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
+
+        if(Input.GetKey(crouchKey))
+        {
+            isCrouching = true;
+        }
+        else
+        {
+            isCrouching = false;
+        }
+
+        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
+    }
+
+    void ControlSpeed()
+    {
+        if (isSprinting && isGrounded)
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, sprintSpeed, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
+        }
+    }
+
+    void ControlDrag()
+    {
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+            rb.AddForce(Vector3.down * gravity * rb.mass);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (hasAuthority)
+        {
+            MovePlayer();
+        }
+    }
+
+    void MovePlayer()
+    {
+        if (isGrounded && !OnSlope())
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if (isGrounded && OnSlope())
+        {
+            rb.AddForce(slopeMoveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded && Input.GetKeyDown(jumpKey))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void Crouch()
+    {
+        if (isCrouching)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, crouchScale, crouchSpeed * Time.deltaTime);
+            movementMultiplier = 6f;
+
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, startingScale, crouchSpeed * Time.deltaTime);
+            movementMultiplier = 10f;
+        }
     }
 }
